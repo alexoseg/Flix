03806853294b911018f315fefce5d270fbf1a9de
Alex Oseguera <alexoseg@fb.com>
2020-06-24 17:56:27 -0700

    User can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView

---
+++ Flix/VIews/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  Flix
+//
+//  Created by Alex Oseguera on 6/24/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
---
+++ Flix/View Controllers/MoviesGridViewController.m
@@ -0,0 +1,104 @@
+//
+//  MoviesGridViewController.m
+//  Flix
+//
+//  Created by Alex Oseguera on 6/24/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "MoviesGridViewController.h"
+#import "MBProgressHUD.h"
+#import "MovieCollectionCell.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface MoviesGridViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation MoviesGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [self fetchMovies];
+    
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *)self.collectionView.collectionViewLayout;
+    
+    layout.minimumInteritemSpacing = 5;
+    layout.minimumLineSpacing = 5; 
+    
+    CGFloat postersPerLine = 2;
+    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (postersPerLine - 1)) / postersPerLine;
+    CGFloat itemHeight = itemWidth * 1.5;
+    layout.itemSize = CGSizeMake(itemWidth, itemHeight); 
+}
+
+-(void)fetchMovies {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=8ec68e637b241eb6bc5b97abcd358733"];
+          NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+          NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+          NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                 if (error != nil) {
+                     NSString *errorMessage = [error localizedDescription];
+                     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot get Movies" message:errorMessage preferredStyle:UIAlertControllerStyleAlert];
+                     UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                         [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+                         [self fetchMovies];
+                     }];
+                     [alert addAction:tryAgainAction];
+                     [self presentViewController:alert animated:YES completion:^{}];
+                 }
+                 else {
+                     NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                     
+                     self.movies = dataDictionary[@"results"];
+                     [self.collectionView reloadData]; 
+                 }
+                [MBProgressHUD hideHUDForView:self.view animated:YES];
+             }];
+          [task resume];
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
+    
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+       
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    
+    cell.posterView.image = nil;
+    [cell.posterView setImageWithURL:posterURL];
+    
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
+
+
+@end
